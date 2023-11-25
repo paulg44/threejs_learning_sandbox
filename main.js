3c28df7f35a7b522a00b6273fb3acdcd11b270cb
paulg44 <paulgarton1984@hotmail.com>
2023-11-25 18:49:52 +0000

    complete reponsive design part of three.js manual

--- main.js
+++ main.js
@@ -53,13 +53,28 @@ function main() {
     makeInstance(geometry, 0xaa8844, 2),
   ];
 
+  // Function to check the canvas size
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio | 0;
+    const width = (canvas.clientWidth * pixelRatio) | 0;
+    const height = (canvas.clientHeight * pixelRatio) | 0;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
   //   Cube spin timings
   function render(time) {
     time *= 0.001;
 
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
 
     cubes.forEach((cube, ndx) => {
       const speed = 1 + ndx * 0.1;
