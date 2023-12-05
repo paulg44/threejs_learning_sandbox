401ba0ddf4e90a30b48fd25c5dfa3d6665424981
paulg44 <paulgarton1984@hotmail.com>
2023-12-05 20:38:50 +0000

    complete light section of three.js manual

--- PastExamples/lights.js
+++ PastExamples/lights.js
@@ -3,11 +3,14 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
+import { RectAreaLightUniformsLib } from "three/addons/lights/RectAreaLightUniformsLib.js";
+import { RectAreaLightHelper } from "three/addons/helpers/RectAreaLightHelper.js";
 
 function main() {
   // Set up canvas
   const canvas = document.querySelector("#c");
   const renderer = new THREE.WebGL1Renderer({ antialias: true, canvas });
+  RectAreaLightUniformsLib.init();
 
   //   Camera options
   const fov = 45;
@@ -43,7 +46,7 @@ function main() {
   texture.repeat.set(repeats, repeats);
 
   const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-  const planeMat = new THREE.MeshPhongMaterial({
+  const planeMat = new THREE.MeshStandardMaterial({
     map: texture,
     side: THREE.DoubleSide,
   });
@@ -55,7 +58,7 @@ function main() {
   {
     const cubeSize = 4;
     const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    const cubeMat = new THREE.MeshPhongMaterial({ color: "#8ac" });
+    const cubeMat = new THREE.MeshStandardMaterial({ color: "#8ac" });
     const mesh = new THREE.Mesh(cubeGeo, cubeMat);
     mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
     scene.add(mesh);
@@ -71,7 +74,7 @@ function main() {
       sphereWidthDivisions,
       sphereHeightDivisions
     );
-    const sphereMat = new THREE.MeshPhongMaterial({ color: "#ca8" });
+    const sphereMat = new THREE.MeshStandardMaterial({ color: "#ca8" });
     const mesh = new THREE.Mesh(sphereGeo, sphereMat);
     mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
     scene.add(mesh);
@@ -90,17 +93,83 @@ function main() {
       this.object[this.prop].set(hexString);
     }
   }
+  class DegRadHelper {
+    constructor(obj, prop) {
+      this.obj = obj;
+      this.prop = prop;
+    }
+    get value() {
+      return THREE.MathUtils.radToDeg(this.obj[this.prop]);
+    }
+    set value(v) {
+      this.obj[this.prop] = THREE.MathUtils.degToRad(v);
+    }
+  }
+
+  function makeXYZGUI(gui, vector3, name, onChangeFn) {
+    const folder = gui.addFolder(name);
+    folder.add(vector3, "x", -10, 10).onChange(onChangeFn);
+    folder.add(vector3, "y", 0, 10).onChange(onChangeFn);
+    folder.add(vector3, "z", -10, 10).onChange(onChangeFn);
+    folder.open();
+  }
 
   {
     //   Lighting
     const color = 0xffffff;
-    const intensity = 1;
-    const light = new THREE.AmbientLight(color, intensity);
+    // Hemisphere lighting variables
+    // const skyColor = 0xb1e1ff;
+    // const groundColor = 0xb97a20;
+    const intensity = 5;
+    const width = 12;
+    const height = 4;
+    const light = new THREE.SpotLight(color, intensity, width, height);
+    light.position.set(0, 10, 0);
+    light.target.position.set(0, 10, 0);
+    light.rotation.x = THREE.MathUtils.degToRad(-90);
     scene.add(light);
+    // scene.add(light.target);
+
+    const helper = new RectAreaLightHelper(light);
+    scene.add(helper);
+
+    // function updateLight() {
+    //   light.target.updateMatrixWorld();
+    //   helper.update();
+    // }
+
+    // updateLight();
 
     const gui = new GUI();
+    // Hemisphere GUI
+    // gui.addColor(new ColorGUIHelper(light, "color"), "value").name("skyColor");
+    // gui
+    //   .addColor(new ColorGUIHelper(light, "groundColor"), "value")
+    //   .name("groundColorColor");
     gui.addColor(new ColorGUIHelper(light, "color"), "value").name("color");
-    gui.add(light, "intensity", 0, 2, 0.01);
+    gui.add(light, "intensity", 0, 10, 0.01);
+    gui.add(light, "width", 0, 20);
+    gui.add(light, "height", 0, 20);
+    // gui.add(light, "distance", 0, 40).onChange(updateLight);
+    gui
+      .add(new DegRadHelper(light.rotation, "x"), "value", -180, 180)
+      .name("x rotation");
+    gui
+      .add(new DegRadHelper(light.rotation, "y"), "value", -180, 180)
+      .name("y rotation");
+    gui
+      .add(new DegRadHelper(light.rotation, "z"), "value", -180, 180)
+      .name("z rotation");
+    // gui
+    //   .add(new DegRadHelper(light, "angle"), "value", 0, 90)
+    //   .name("angle")
+    //   .onChange(updateLight);
+    // gui.add(light, "penumbra", 0, 1, 0.01);
+    // gui.add(light.target.position, "x", -10, 10);
+    // gui.add(light.target.position, "z", -10, 10);
+    // gui.add(light.target.position, "y", 0, 10);
+    makeXYZGUI(gui, light.position, "position");
+    // makeXYZGUI(gui, light.target.position, "target", updateLight);
   }
 
   function resizeRendererToDisplaySize(renderer) {
